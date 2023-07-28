f67223ffc2d1db9354cccdf22d74ecff40ae23c9
Bhuwan <bhuwan.singh@crossml.com>
2023-07-28 11:44:13 +0530

    project complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Menu from "./pages/Menu";
-import Pagenotfound from "./pages/Pagenotfound";
+import Pricing from "./pages/Pricing";
 
 
 function App() {
@@ -11,11 +7,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="*" element={<Pagenotfound />} />
+          <Route path="/" element={<Pricing />} />
         </Routes>
       </BrowserRouter>
     </div>
